b41c03dc75911ab1c9ad1abe96051b22ca767a60
maayamancio <mayarazaltrao@gmail.com>
2026-03-08 13:44:24 -0300

    Add file variaveis2
    Para preencher as informações

---
+++ LogicaComPython/Variaveis2.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ") # Solicita ao usuário que digite seu nome e armazena na variável 'nome'
+idade = int(input("Digite sua idade: ")) # Solicita ao usuário que digite sua idade, converte para inteiro e armazena na variável 'idade'
+altura = float(input("Digite sua altura (em metros): ")) # Solicita ao usuário que
+
+print(f"Nome: {nome}, Idade: {idade}, Altura: {altura}m") # Exibe as informações fornecidas pelo usuário
+
+
