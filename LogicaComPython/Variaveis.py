e9ff9f7f503afba0fe00bf984f2ea52797784734
maayamancio <mayarazaltrao@gmail.com>
2026-03-08 13:40:56 -0300

    Add variaveis

---
+++ LogicaComPython/Variaveis.py
@@ -0,0 +1,9 @@
+# Variáveis e tipos de dados "básicos"
+# Uma variável é um espaço na memória do computador onde podemos armazenar um valor.
+
+nome = "Mayara" # String (texto)
+idade = 30 # var do tipo Inteiro (número inteiro)
+altura = 1.65 # var do tipo Float (número decimal)
+Engenheira = True # var do tipo Booleano (verdadeiro ou falso)
+
+print(f"Nome: {nome}, Idade: {idade}, Altura: {altura}, Engenheira: {Engenheira}")    
